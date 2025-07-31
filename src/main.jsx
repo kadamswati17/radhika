6b6f96435a30d076bceffede887cb407cca48969
Swati kadam <swatimarotikadam@gmail.com>
2025-07-31 16:59:05 +0530

    file changed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/radhika-herbal-react">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>
